f124ac3fdf364a7afd1bc657ab7c75f9380b8367
rohansalo <157637337+rohansalo@users.noreply.github.com>
2024-02-12 21:56:54 +0530

    yo

--- shellscripting.sh
+++ shellscripting.sh
@@ -2,3 +2,7 @@
 echo "hello world! iam learning devops with aws"
 echo "hey i am rohan"
 echo "how are you"
+person1 =Ram
+person2=shyam
+echo "$person1:Hi $person2 i am good"
+echo "$person2: hello $person1 thats well"
