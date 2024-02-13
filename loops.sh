8a2885cd98f13405d19fc13b510d1c1e8d459740
rohansalo <157637337+rohansalo@users.noreply.github.com>
2024-02-13 21:12:56 +0530

    yo

--- loops.sh
+++ loops.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 USERID=$(id -u)
+R="\e[31m"
+N="\e[0m"
 
-
-if($USERID -ne 0);
+if[$USERID -ne 0];
 then
-    echo "Error::please run this script with root access"
+    echo -e "$R Error::please run this script with root access $N"
     exit 1
 fi
